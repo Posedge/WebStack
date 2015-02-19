d8bfa9568a9b7b54078de9fe31c4cf5df688d485
Posedge <valentin.trifonov@hotmail.de>
2015-02-19 19:10:25 +0100

    implemented reordering of tabs and frames, and included jquery in the repo

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ This chrome extension adds a stack for your tabs in chrome, allowing a workflow 
 
 ### Installation
 
-As of now, you can only install this for chrome locally. To do that, download and place jquery 2.1.3 (http://jquery.com/download/) in the WebStack folder, enable developer mode at the chrome extensions page, select "Load unpacked extension" and navigate to the WebStack folder.
+As of now, you can only install this for chrome locally. To do that, enable developer mode at the chrome extensions page, select "Load unpacked extension" and navigate to the WebStack folder.
 
 ### TODO list
 
 - [x] Ignore empty tabs
 - [ ] Fix favicon when pushed before the site has loaded
 - [ ] Allow deleting/opening tabs
-- [ ] Allow reordering of Tabs
+- [x] Allow reordering of Tabs
 - [ ] Add Badge displaying number of items
 - [ ] Save and restore the browsing history with the tab
 - [ ] Add proper icon(s) and stylesheets
--- WebStack/popup.js
+++ WebStack/popup.js
@@ -21,14 +21,79 @@ function renderStack(stack){
   // draw stack in reverse order as the last element represents the top
   for(var i = stack.frames.length - 1; i >= 0; i--){
     frame = stack.frames[i];
-    elementContent += '<div class="stack-frame">';
+    elementContent += '<div class="stack-frame" id="frame-' + i + '">';
+    //frame.tabObjects.forEach(function(tab){
     for(var j = 0; j < frame.tabObjects.length; j++){
-      elementContent += '<div class="tab-favicon-container" title="' + frame.tabObjects[j].title + '"><img class="tab-favicon-img" src="' + frame.tabObjects[j].favUrl + '" alt="' + frame.tabObjects[j].title + '" /></div>';
-    }
+      tab = frame.tabObjects[j];
+      elementContent += '<div class="tab-favicon-container" title="' + tab.title + '" id="frame-' + i + '-tab-' + j + '">';
+      elementContent += '<img class="tab-favicon-img" src="' + tab.favUrl + '" alt="' + tab.title + '" />';
+      elementContent += '</div>';
+    };
     elementContent += '</div>';
   }
   
   $("#stack").html(elementContent);
+
+  $("#stack").sortable({
+    update: rebuildStackFromHtml
+  });
+
+  $(".stack-frame").sortable({
+    connectWith: ".stack-frame",
+    //stop: rebuildStackFromHtml,
+    update: rebuildStackFromHtml
+    /*function(event, ui){
+      console.log("sortable-start");
+      getStack(function(stack){
+        stack.frames.push({tabObjects: []});
+        setStack(stack, function(){
+          renderStack(stack);
+        });
+      });
+    }*/
+  });
+
+  $(".stack-frame").disableSelection();
+  $(".tab-favicon-container").disableSelection();
+}
+
+/**
+ * Read frames and tabs, and update them in the stack object.
+ * Called, when the user changes the order of the frames or tabs (through drag-and-drop).
+ */
+function rebuildStackFromHtml(){
+  // TODO for some reason, this is sometimes called TWICE. Fix this (but it works fine though)
+  var framesArray = $("#stack").sortable("toArray");
+
+  // re-build stack in new order.
+  getStack(function(stack){
+    var newFrames = [];
+    for (var i = framesArray.length - 1; i >= 0; i--){
+      // again, reverse order because the first element in the html is the top of the stack
+      var tabsArray = $("#" + framesArray[i]).sortable("toArray");
+
+      var tabObjects = [];
+      for (var j = 0; j < tabsArray.length; j++){
+        var parsedId = tabsArray[j].split("-");
+        if(parsedId.length != 4 || parsedId[0] !== "frame" || parsedId[2] !== "tab"){
+          console.log("WebStack: Reorder failure!");
+          return;
+        }
+
+        var f = parseInt(parsedId[1]);
+        var t = parseInt(parsedId[3]);
+        tabObjects.push(stack.frames[f].tabObjects[t]);
+      }
+
+      if(tabObjects.length != 0)
+        newFrames.push({tabObjects: tabObjects});
+    }
+
+    var newStack = {frames: newFrames};
+    setStack(newStack, function(){
+      renderStack(newStack); // re-rendering wouldn't be necessary, but for now I will leave this in here in case the order was wrong.
+    });
+  });
 }
 
 /**
@@ -124,7 +189,10 @@ function del(){
 }
 
 document.addEventListener('DOMContentLoaded', function(){
+  // draw stack
   getStack(function(stack){renderStack(stack);});
+
+  // event listeners
   $("#push-button").click(push);
   $("#pop-button").click(pop);
 });
